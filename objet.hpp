a96901446db59abac309c80ca355843d16b5dd21
sonia <soniiahamm@gmail.com>
2023-06-20 16:28:44 +0200

    first

---
+++ objet.hpp
@@ -0,0 +1,16 @@
+class produit
+{
+    std :: string marque;
+    magasin s_magasin;
+    int taille;
+    int nb_restant;
+    std :: string couleur;
+    std :: string type;
+    std :: string genre;
+}; 
+class magasin
+{
+    std :: string marque; 
+    int code_postale;
+    std :: string pays;
+};
